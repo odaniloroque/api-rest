2830cbbb98de5323a7792f7a147d1812c25be0e3
Danilo Roque <dsr.proj3ct@gmail.com>
2022-11-14 17:58:50 -0300

    Implementado rotas de produtos

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,48 +2,78 @@ const express = require("express");
 const router = express.Router();
 const mysql = require('../mysql').pool;
 
-router.get('/', (req, res, next) =>{
-    res.status(200).send({
-        mensagem: 'Rota de GET'
-    })
-});
-
-router.get('/:id_produto', (req,res,next) =>{
-    res.status(200).send({
-        mensagem: 'Rota de GET by ID'
-    })
-});
-
-router.post('/', (req,res,next) =>{
-
+router.get('/', (req,res,next) => {
     mysql.getConnection((error, conn)=>{
-        if(error) {return res.status(400).send({error: error});}
+        if(error) {return res.status(500).send({error: error})}
         conn.query(
-            'INSERT INTO produtos (nome, preco) VALUES (?,?)'),
+            'SELECT * FROM produtos',
+            (err, resultado, fields) => {
+                if (err) { return res.status(500).send({error: err})}
+                return res.status(200).send({response: resultado})
+            }
+        )
+    });
+});
+
+router.get('/:id_produto', (req,res,next) => {
+    mysql.getConnection((error, conn)=>{
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produtos = ?',
+            [req.params.id_produto],
+            (err, resultado, fields) => {
+                if (err) { return res.status(500).send({error: err})}
+                return res.status(200).send({response: resultado})
+            }
+        )
+    });
+});
+
+router.post('/', (req,res,next) => {
+    mysql.getConnection((error, conn)=>{
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
             [req.body.nome, req.body.preco],
-            (error, resultado, field) => {
+            (err, resultado, field) => {
                 conn.release();
-                if (error) {
+                if (err) {
                     return res.status(500).send({
-                            error: error, 
+                            error: err, 
                             response:null
                     });
                 }
                 res.status(201).send({
                     mensagem: 'Produto inserido com sucesso',
                     id_produto: resultado.insertId
-                })
-        }
-    })
+                });
+            }
+        )
+    });
+});
+
    
-});
 
-router.patch('/', (req,res,next) =>{
-    res.status(200).send({
-        mensagem: 'Rota de PATCH'
-    })
+router.patch('', (req,res,next) => {
+    mysql.getConnection((error, conn)=>{
+        if(error) {return res.status(500).send({error: error})}
+        conn.query(
+            `UPDATE produtos 
+            SET nome = ?
+                preco = ?
+            WHERE id_produtos = ?`,
+            [req.body.nome, 
+                req.body.preco,
+                req.body.id_produtos],
+            (err, resultado, fields) => {
+                if (err) { return res.status(500).send({error: err})}
+                res.status(201).send({
+                    mensagem: 'Cadastro Atualizado'
+                })
+            }
+        )
+    });
 });
-
 router.delete('/', (req,res,next) =>{
     res.status(200).send({
         mensagem: 'Rota de DELETE'
