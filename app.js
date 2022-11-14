5abe10bed5ee31f6cd297530ee10fd6e7d03c94d
Danilo Roque <dsr.proj3ct@gmail.com>
2022-11-14 16:31:11 -0300

    Atualizacao de Codigo

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express =  require('express');
 const app = express();
-
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const rotaTeste = require('./routes/teste');
 const rotaProtudos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
-const morgan = require('morgan');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '#');
+    res.header('Access-Control-Allow-Header',
+                'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+                );
+    if(req.method ==='OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).send({});
+    }
+    next();
+})
 app.use('/test',rotaTeste);
 app.use('/produtos',rotaProtudos);
 app.use('/pedidos', rotaPedidos);
@@ -20,8 +34,8 @@ app.use((req, res, next)=>{
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
-    return res.send({erro:{mensagem: error.mensage}})
-})
+    return res.send({erro:{mensagem: error.mensage}});
+});
 
 
 module.exports = app;
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) =>{
     res.status(200).send({
@@ -14,9 +15,27 @@ router.get('/:id_produto', (req,res,next) =>{
 });
 
 router.post('/', (req,res,next) =>{
-    res.status(200).send({
-        mensagem: 'Rota de POST'
+
+    mysql.getConnection((error, conn)=>{
+        if(error) {return res.status(400).send({error: error});}
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?,?)'),
+            [req.body.nome, req.body.preco],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                            error: error, 
+                            response:null
+                    });
+                }
+                res.status(201).send({
+                    mensagem: 'Produto inserido com sucesso',
+                    id_produto: resultado.insertId
+                })
+        }
     })
+   
 });
 
 router.patch('/', (req,res,next) =>{
