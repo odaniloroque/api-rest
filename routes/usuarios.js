0335f081a4b75e3eaea183cff28687104e766e47
Danilo Roque <dsr.proj3ct@gmail.com>
2022-11-16 11:15:31 -0300

    melhoria na rota de usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,27 +6,36 @@ const bcrypt = require('bcrypt');
 router.post('/cadastro', (req, res, next) => {
     mysql.getConnection((err, conn) => {
         if(err){return res.status(500).send({error: err})}
-        bcrypt.hash(req.body.senha, 10, (errBrypt, hash)=> {
-            if(errBrypt) {return res.status(500).send({error: errBrypt})}
-            conn.query = `INSERT INTO usuarios (email, senha) VALUES (?,?)`,
-            [
-                req.body.email,
-                hash
-            ],
-            (err, resultado, field) => {
-                conn.release();
-                if(err){return res.status(500).send({error: err})}
-                response = {                    
-                    mensagem: 'Usuario Cadastro com sucesso.',
-                    usuarioCriado: {
-                        id_usuario: resultado.insertId,
-                        email: req.body.email
+        conn.query("SELECT * FROM USUARIO WHERE email = ?",[req.body.email],(error, resultado)=> {
+            if(resultado.length > 0) {
+                return res.status(401).send({mensagem: 'Usuarios já cadastrado'})
+            } else
+            {
+                bcrypt.hash(req.body.senha, 10, (errBrypt, hash)=> {
+                    if(errBrypt) {return res.status(500).send({error: errBrypt})}
+                    conn.query = `INSERT INTO usuarios (email, senha) VALUES (?,?)`,
+                    [
+                        req.body.email,
+                        hash
+                    ],
+                    (err, resultado, field) => {
+                        conn.release();
+                        if(err){return res.status(500).send({error: err})}
+                        response = {                    
+                            mensagem: 'Usuario Cadastro com sucesso.',
+                            usuarioCriado: {
+                                id_usuario: resultado.insertId,
+                                email: req.body.email
+                            }
+                        }
+                        return res.status(201).send({response})
                     }
-                }
-                return res.status(201).send({response})
-            }
+        
+                });
 
-        });
+            }
+        })  
+       
         
 
     })
